14e13392bf3e2fb6f7ec9dc50e36a199aa771e09
dush valentin <codesvalentin@gmail.com>
2024-10-04 13:46:25 +0200

    fix: optimize imports

--- index.mjs
+++ index.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { writeFileSync } from 'fs'
-import path from 'path'
 import { program } from 'commander'
 import Groq from 'groq-sdk'
 import Configstore from 'configstore'
